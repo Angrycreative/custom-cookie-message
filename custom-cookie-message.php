e13afaa2936fffd3fb70c15f601ed9d495115a46
Luigi Guevara <luigi.guevara@angrycreative.se>
2017-08-08 11:19:31 +0200

    Applied Code Standard

--- custom-cookie-message.php
+++ custom-cookie-message.php
@@ -10,21 +10,19 @@
  * Domain: cookie-message
  **/
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-define('CUSTOM_COOKIE_MESSAGE_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
-define('CUSTOM_COOKIE_MESSAGE_PLUGIN_URL', plugins_url('', __FILE__).'/');
+define( 'CUSTOM_COOKIE_MESSAGE_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'CUSTOM_COOKIE_MESSAGE_PLUGIN_URL', plugins_url( '', __FILE__ ) . '/' );
 
-define('CUSTOM_COOKIE_MESSAGE_PLUGIN_SLUG', 'custom-cookie-message-list');
+define( 'CUSTOM_COOKIE_MESSAGE_PLUGIN_SLUG', 'custom-cookie-message-list' );
 
-require_once('includes/ac-cookie-message.php');
+require_once( 'includes/ac-cookie-message.php' );
 
-add_action( 'plugins_loaded', function() {
-
-    $GLOBALS['CustomCookie'] = new AC_Custom_Cookie_Message(); 
-
-});
-
+add_action( 'plugins_loaded', function () {
+	$GLOBALS['CustomCookie'] = new AC_Custom_Cookie_Message();
+} );
 
 register_activation_hook( __FILE__, array( 'AC_Custom_Cookie_Message', 'plugin_activation' ) );
-
--- js/ac-custom-cookie-message-backend.js
+++ js/ac-custom-cookie-message-backend.js
@@ -3,51 +3,51 @@
  * Date: 2016-10-10.
  */
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-    $(function() {
+	$(function () {
 
-        // Add Color Picker to all inputs that have 'color-field' class
-        $( '.cpa-color-picker' ).wpColorPicker();
+		// Add Color Picker to all inputs that have 'color-field' class
+		$('.cpa-color-picker').wpColorPicker();
 
-        $( "#opacity_slider" ).slider({
-            range: "min",
-            value: parseInt($("#opacity_slider_amount").val()),
-            min: 50,
-            max: 100,
-            slide: function( event, ui ) {
-                $( "#opacity_slider_amount" ).val( ui.value + "%");
-            }
-        });
+		$("#opacity_slider").slider({
+			range: "min",
+			value: parseInt($("#opacity_slider_amount").val()),
+			min: 50,
+			max: 100,
+			slide: function (event, ui) {
+				$("#opacity_slider_amount").val(ui.value + "%");
+			}
+		});
 
-        $( "#message_height_slider" ).slider({
-            range: "min",
-            value: parseInt($("#message_height_slider_amount").val()),
-            min: 5,
-            max: 40,
-            slide: function( event, ui ) {
-                $( "#message_height_slider_amount" ).val( ui.value );
-            }
-        });
+		$("#message_height_slider").slider({
+			range: "min",
+			value: parseInt($("#message_height_slider_amount").val()),
+			min: 5,
+			max: 40,
+			slide: function (event, ui) {
+				$("#message_height_slider_amount").val(ui.value);
+			}
+		});
 
-        $( "#button_height_slider" ).slider({
-            range: "min",
-            value: parseInt($("#button_height_slider_amount").val()),
-            min: 5,
-            max: 40,
-            slide: function( event, ui ) {
-                $( "#button_height_slider_amount" ).val( ui.value );
-            }
-        });
+		$("#button_height_slider").slider({
+			range: "min",
+			value: parseInt($("#button_height_slider_amount").val()),
+			min: 5,
+			max: 40,
+			slide: function (event, ui) {
+				$("#button_height_slider_amount").val(ui.value);
+			}
+		});
 
-        $( "#button_width_slider" ).slider({
-            range: "min",
-            value: parseInt($("#button_width_slider_amount").val()),
-            min: 5,
-            max: 40,
-            slide: function( event, ui ) {
-                $( "#button_width_slider_amount" ).val( ui.value );
-            }
-        });
-    });
+		$("#button_width_slider").slider({
+			range: "min",
+			value: parseInt($("#button_width_slider_amount").val()),
+			min: 5,
+			max: 40,
+			slide: function (event, ui) {
+				$("#button_width_slider_amount").val(ui.value);
+			}
+		});
+	});
 });
