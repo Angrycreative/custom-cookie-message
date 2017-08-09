12b29479df00ad6383bb79171402869f55ec9648
Luigi Guevara <luigi.guevara@angrycreative.se>
2017-08-09 13:21:07 +0200

    Moving code to list cookies

--- js/ac-custom-cookie-message-backend.js
+++ js/ac-custom-cookie-message-backend.js
@@ -3,51 +3,54 @@
  * Date: 2016-10-10.
  */
 
-jQuery( document ).ready( function ( $ ) {
+jQuery( function ( $ ) {
 
-	$( function () {
+	function getCookies () {
+		var pairs = document.cookie.split(";");
 
-		// Add Color Picker to all inputs that have 'color-field' class
-		$( '.cpa-color-picker' ).wpColorPicker();
 
-		$( "#opacity_slider" ).slider( {
-			range: "min",
-			value: parseInt( $( "#opacity_slider_amount" ).val() ),
-			min: 50,
-			max: 100,
-			slide: function ( event, ui ) {
-				$( "#opacity_slider_amount" ).val( ui.value + "%" );
-			}
-		} );
+	}
 
-		$( "#message_height_slider" ).slider( {
-			range: "min",
-			value: parseInt( $( "#message_height_slider_amount" ).val() ),
-			min: 5,
-			max: 40,
-			slide: function ( event, ui ) {
-				$( "#message_height_slider_amount" ).val( ui.value );
-			}
-		} );
+	// Add Color Picker to all inputs that have 'color-field' class
+	$( '.cpa-color-picker' ).wpColorPicker();
 
-		$( "#button_height_slider" ).slider( {
-			range: "min",
-			value: parseInt( $( "#button_height_slider_amount" ).val() ),
-			min: 5,
-			max: 40,
-			slide: function ( event, ui ) {
-				$( "#button_height_slider_amount" ).val( ui.value );
-			}
-		} );
+	$( "#opacity_slider" ).slider( {
+		range: "min",
+		value: parseInt( $( "#opacity_slider_amount" ).val() ),
+		min: 50,
+		max: 100,
+		slide: function ( event, ui ) {
+			$( "#opacity_slider_amount" ).val( ui.value + "%" );
+		}
+	} );
 
-		$( "#button_width_slider" ).slider( {
-			range: "min",
-			value: parseInt( $( "#button_width_slider_amount" ).val() ),
-			min: 5,
-			max: 40,
-			slide: function ( event, ui ) {
-				$( "#button_width_slider_amount" ).val( ui.value );
-			}
-		} );
+	$( "#message_height_slider" ).slider( {
+		range: "min",
+		value: parseInt( $( "#message_height_slider_amount" ).val() ),
+		min: 5,
+		max: 40,
+		slide: function ( event, ui ) {
+			$( "#message_height_slider_amount" ).val( ui.value );
+		}
+	} );
+
+	$( "#button_height_slider" ).slider( {
+		range: "min",
+		value: parseInt( $( "#button_height_slider_amount" ).val() ),
+		min: 5,
+		max: 40,
+		slide: function ( event, ui ) {
+			$( "#button_height_slider_amount" ).val( ui.value );
+		}
+	} );
+
+	$( "#button_width_slider" ).slider( {
+		range: "min",
+		value: parseInt( $( "#button_width_slider_amount" ).val() ),
+		min: 5,
+		max: 40,
+		slide: function ( event, ui ) {
+			$( "#button_width_slider_amount" ).val( ui.value );
+		}
 	} );
 } );
