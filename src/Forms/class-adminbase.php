f841e100025b4e71370cde754c631ee260189a9d
Luigi Guevara <luigi.guevara@angrycreative.se>
2017-12-01 17:00:44 +0100

    Adminbase options

--- src/Forms/class-adminbase.php
+++ src/Forms/class-adminbase.php
@@ -15,12 +15,22 @@ namespace CustomCookieMessage\Forms;
 class AdminBase {
 
 	/**
+	 * Option Group.
+	 *
+	 * @var string
+	 */
+	protected $option_group = 'custom_cookie_message';
+
+	/**
 	 * Custom Cookie Message options.
 	 *
 	 * @var array
 	 */
 	protected $options;
 
+	/**
+	 * AdminBase constructor.
+	 */
 	public function __construct() {
 		$this->options = get_option( 'custom_cookie_message' );
 	}
