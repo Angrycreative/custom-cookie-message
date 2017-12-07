9a8d319c62263b013dd84c289264e62290d60bf6
Luigi Guevara <luigi.guevara@angrycreative.se>
2017-12-07 16:58:18 +0100

    Template and admin fix

--- src/Forms/class-adminstylingoptions.php
+++ src/Forms/class-adminstylingoptions.php
@@ -60,27 +60,27 @@ class AdminStylingOptions extends AdminBase {
 
 		add_settings_section( 'styling', esc_html__( 'Styling Options', 'custom-cookie-message' ), [ $this, 'cookies_styling_options_callback' ], $this->section_page );
 
-		add_settings_field( 'cookies_styling_options', esc_html__( 'Message container background', 'custom-cookie-message' ), [ $this, 'cookies_message_color_picker_callback' ], $this->section_page, 'styling' );
+		add_settings_field( 'message_color_picker', esc_html__( 'Message container background', 'custom-cookie-message' ), [ $this, 'cookies_message_color_picker_callback' ], $this->section_page, 'styling' );
 
-		add_settings_field( 'Message Size', esc_html__( 'Message container padding top and bottom', 'custom-cookie-message' ), [ $this, 'cookies_message_height_slider_callback' ], $this->section_page, 'styling' );
+		add_settings_field( 'message_height_slider_amount', esc_html__( 'Message container padding top and bottom', 'custom-cookie-message' ), [ $this, 'cookies_message_height_slider_callback' ], $this->section_page, 'styling' );
 
-		add_settings_field( 'Opacity', esc_html__( 'Message container opacity', 'custom-cookie-message' ), [ $this, 'cookies_opacity_slider_callback' ], $this->section_page, 'styling' );
+		add_settings_field( 'opacity_slider_amount', esc_html__( 'Message container opacity', 'custom-cookie-message' ), [ $this, 'cookies_opacity_slider_callback' ], $this->section_page, 'styling' );
 
 		add_settings_field( 'text_font', esc_html__( 'Text font', 'custom-cookie-message' ), [ $this, 'cookies_text_font_callback' ], $this->section_page, 'styling' );
 
-		add_settings_field( 'Text Color', esc_html__( 'Text Color', 'custom-cookie-message' ), [ $this, 'cookies_text_color_picker_callback' ], $this->section_page, 'styling' );
+		add_settings_field( 'text_color_picker', esc_html__( 'Text Color', 'custom-cookie-message' ), [ $this, 'cookies_text_color_picker_callback' ], $this->section_page, 'styling' );
 
-		add_settings_field( 'Link Color', esc_html__( 'Link Color', 'custom-cookie-message' ), [ $this, 'cookies_link_color_picker_callback' ], $this->section_page, 'styling' );
+		add_settings_field( 'link_color_picker', esc_html__( 'Link Color', 'custom-cookie-message' ), [ $this, 'cookies_link_color_picker_callback' ], $this->section_page, 'styling' );
 
 		add_settings_field( 'add_button_class', esc_html__( 'Button classes', 'custom-cookie-message' ), [ $this, 'cookies_add_button_class_callback' ], $this->section_page, 'styling' );
 
-		add_settings_field( 'Button Color', esc_html__( 'Button Color', 'custom-cookie-message' ), [ $this, 'cookies_button_color_picker_callback' ], $this->section_page, 'styling' );
+		add_settings_field( 'button_color_picker', esc_html__( 'Button Color', 'custom-cookie-message' ), [ $this, 'cookies_button_color_picker_callback' ], $this->section_page, 'styling' );
 
-		add_settings_field( 'Button Hover Color', esc_html__( 'Button Hover Color', 'custom-cookie-message' ), [ $this, 'cookies_button_hover_color_picker_callback' ], $this->section_page, 'styling' );
+		add_settings_field( 'button_hover_color_picker', esc_html__( 'Button Hover Color', 'custom-cookie-message' ), [ $this, 'cookies_button_hover_color_picker_callback' ], $this->section_page, 'styling' );
 
-		add_settings_field( 'cookies_styling_options', esc_html__( 'Button Text Color', 'custom-cookie-message' ), [ $this, 'cookies_button_text_color_picker_callback' ], $this->section_page, 'styling' );
+		add_settings_field( 'button_text_color_picker', esc_html__( 'Button Text Color', 'custom-cookie-message' ), [ $this, 'cookies_button_text_color_picker_callback' ], $this->section_page, 'styling' );
 
-		add_settings_field( 'Button_height', esc_html__( 'Button Height', 'custom-cookie-message' ), [ $this, 'cookies_button_height_slider_callback' ], $this->section_page, 'styling' );
+		add_settings_field( 'button_height_slider_amount', esc_html__( 'Button Height', 'custom-cookie-message' ), [ $this, 'cookies_button_height_slider_callback' ], $this->section_page, 'styling' );
 
 		add_settings_field( 'button_width_slider_amount', esc_html__( 'Button Width', 'custom-cookie-message' ), [ $this, 'cookies_button_width_slider_callback' ], $this->section_page, 'styling' );
 	}
@@ -93,7 +93,7 @@ class AdminStylingOptions extends AdminBase {
 	}
 
 	/**
-	 * Color Picker field.
+	 * Background color Picker field.
 	 */
 	public function cookies_message_color_picker_callback() {
 		$val = isset( $this->options['styles']['message_color_picker'] ) ? $this->options['styles']['message_color_picker'] : '';
@@ -118,49 +118,76 @@ class AdminStylingOptions extends AdminBase {
 		echo '<div id="opacity_slider" class="slider"><div id="opacity_slider_handle" class="ui-slider-handle ui-slider-handle-custom"></div></div>';
 	}
 
-	public function cookies_button_color_picker_callback() {
-		$val = isset( $this->options['styles']['button_color_picker'] ) ? $this->options['styles']['button_color_picker'] : '';
-		echo '<input type="text" id="button_color_picker" name="custom_cookie_message[styles][button_color_picker]" value="' . $val . '" class="cpa-color-picker" >'; // WPCS: XSS ok.
-	}
-
-	public function cookies_button_hover_color_picker_callback() {
-		$val = isset( $this->options['styles']['button_hover_color_picker'] ) ? $this->options['styles']['button_hover_color_picker'] : '';
-		echo '<input type="text" id="button_hover_color_picker" name="custom_cookie_message[styles][button_hover_color_picker]" value="' . $val . '" class="cpa-color-picker" >'; // WPCS: XSS ok.
-	}
-
-	public function cookies_button_text_color_picker_callback() {
-		$val = $this->options['styles']['button_text_color_picker'];
-		echo '<input type="text" id="button_text_color_picker" name="custom_cookie_message[styles][button_text_color_picker]" value="' . $val . '" class="cpa-color-picker" >'; // WPCS: XSS ok.
-	}
-
-	public function cookies_text_color_picker_callback() {
-		$val = isset( $this->options['styles']['text_color_picker'] ) ? $this->options['styles']['text_color_picker'] : '';
-		echo '<input type="text" id="text_color_picker" name="custom_cookie_message[styles][text_color_picker]" value="' . $val . '" class="cpa-color-picker" >'; // WPCS: XSS ok.
-	}
-
-	public function cookies_link_color_picker_callback() {
-		$val = isset( $this->options['styles']['link_color_picker'] ) ? $this->options['styles']['link_color_picker'] : '';
-		echo '<input type="text" id="link_color_picker" name="custom_cookie_message[styles][link_color_picker]" value="' . $val . '" class="cpa-color-picker" >'; // WPCS: XSS ok.
-	}
-
+	/**
+	 * Text font family.
+	 */
 	public function cookies_text_font_callback() {
 		$val = isset( $this->options['styles']['text_font'] ) ? $this->options['styles']['text_font'] : '';
 		echo '<input type="text" id="text_font" name="custom_cookie_message[styles][text_font]" value="' . $val . '" />'; // WPCS: XSS ok.
 		echo '<div><p>Replace your standard paragraph font-family. Leave empty for the standard font-family</p></div>';
 	}
 
+	/**
+	 * Color Text.
+	 */
+	public function cookies_text_color_picker_callback() {
+		$val = isset( $this->options['styles']['text_color_picker'] ) ? $this->options['styles']['text_color_picker'] : '';
+		echo '<input type="text" id="text_color_picker" name="custom_cookie_message[styles][text_color_picker]" value="' . $val . '" class="cpa-color-picker" >'; // WPCS: XSS ok.
+	}
+
+	/**
+	 * Color link.
+	 */
+	public function cookies_link_color_picker_callback() {
+		$val = isset( $this->options['styles']['link_color_picker'] ) ? $this->options['styles']['link_color_picker'] : '';
+		echo '<input type="text" id="link_color_picker" name="custom_cookie_message[styles][link_color_picker]" value="' . $val . '" class="cpa-color-picker" >'; // WPCS: XSS ok.
+	}
+
+	/**
+	 * Button Class.
+	 */
 	public function cookies_add_button_class_callback() {
 		$val = isset( $this->options['styles']['add_button_class'] ) ? $this->options['styles']['add_button_class'] : '';
 		echo '<input type="text" id="add_button_class" name="custom_cookie_message[styles][add_button_class]" value="' . $val . '" />'; // WPCS: XSS ok.
 		echo '<div><p>Replace the standard styling of the button by specifying your own class. If several classes, separate with space. Leave empty to keep the standard styling.</p></div>';
 	}
 
+	/**
+	 * Button color.
+	 */
+	public function cookies_button_color_picker_callback() {
+		$val = isset( $this->options['styles']['button_color_picker'] ) ? $this->options['styles']['button_color_picker'] : '';
+		echo '<input type="text" id="button_color_picker" name="custom_cookie_message[styles][button_color_picker]" value="' . $val . '" class="cpa-color-picker" >'; // WPCS: XSS ok.
+	}
+
+	/**
+	 * Button hover color.
+	 */
+	public function cookies_button_hover_color_picker_callback() {
+		$val = isset( $this->options['styles']['button_hover_color_picker'] ) ? $this->options['styles']['button_hover_color_picker'] : '';
+		echo '<input type="text" id="button_hover_color_picker" name="custom_cookie_message[styles][button_hover_color_picker]" value="' . $val . '" class="cpa-color-picker" >'; // WPCS: XSS ok.
+	}
+
+	/**
+	 * Button text color.
+	 */
+	public function cookies_button_text_color_picker_callback() {
+		$val = $this->options['styles']['button_text_color_picker'];
+		echo '<input type="text" id="button_text_color_picker" name="custom_cookie_message[styles][button_text_color_picker]" value="' . $val . '" class="cpa-color-picker" >'; // WPCS: XSS ok.
+	}
+
+	/**
+	 * Button height.
+	 */
 	public function cookies_button_height_slider_callback() {
 		$val = isset( $this->options['styles']['button_height_slider_amount'] ) ? $this->options['styles']['button_height_slider_amount'] : '5';
 		echo '<input type="text" id="button_height_slider_amount" name="custom_cookie_message[styles][button_height_slider_amount]" value="' . $val . '" readonly class="hidden">'; // WPCS: XSS ok.
 		echo '<div id="button_height_slider" class="slider"><div id="button_height_handle" class="ui-slider-handle ui-slider-handle-custom"></div></div>';
 	}
 
+	/**
+	 * Button width.
+	 */
 	public function cookies_button_width_slider_callback() {
 		$val = isset( $this->options['styles']['button_width_slider_amount'] ) ? $this->options['styles']['button_width_slider_amount'] : '10';
 		echo '<input type="text" id="button_width_slider_amount" name="custom_cookie_message[styles][button_width_slider_amount]" value="' . $val . '" readonly class="hidden">';
--- templates/cookie-notice.php
+++ templates/cookie-notice.php
@@ -1,19 +1,35 @@
 <?php
+/**
+ * Template cookie notice.
+ *
+ * @package CustomCookieMessage.
+ */
+
 $options = get_option( 'custom_cookie_message' );
+
+list( $r, $g, $b ) = sscanf( $options['styles']['message_color_picker'], '#%02x%02x%02x' );
+$background_opacity  = $options['styles']['opacity_slider_amount'] / 100;
+$style_notice_banner = "background-color: rgba({$r}, {$g}, {$b}, {$background_opacity});";
+$style_notice_banner .= ' padding: ' . $options['styles']['message_height_slider_amount'] . 'px 0;';
+
+$style_message = 'color: ' . $options['styles']['text_color_picker'] . ';';
+$style_message .= empty( $options['styles']['text_font'] ) ? '' : 'font-family: ' . $options['styles']['text_font'] . ';';
+
+$style_link = 'color: ' . $options['styles']['link_color_picker'] . ';';
+
+$style_button_class = $options['styles']['add_button_class'];
+
 ?>
-<div id="custom-cookie-message-banner" class="custom-cookie-message-banner custom-cookie-message-banner--<?php echo esc_attr( $options['general']['location_options'] ); ?>">
-	<div class="cookie-block"></div>
-	<div class="cookie-content">
-		<div class="warning-text">
-			<p <?php echo empty( $options['styles']['text_font'] ) ? esc_attr( '' ) : esc_attr( 'style="font-family: ' . $options['styles']['text_font'] ) . '"'; ?> ><?php esc_html_e( $options['content']['textarea_warning_text'], 'custom-cookie-message' ); ?>
-				<a style="color: <?php echo esc_attr( $options['styles']['link_color_picker'] ); ?>" href=" <?php echo esc_url( $options['general']['cookies_page_link'] ); ?>"><?php esc_html_e( $options['content']['input_link_text'], 'custom-cookie-message' ); ?></a>
+<div id="custom-cookie-message-banner" class="custom-cookie-message-banner custom-cookie-message-banner--<?php echo esc_attr( $options['general']['location_options'] ); ?>" style="<?php echo esc_attr( $style_notice_banner ); ?>">
+	<div class="custom-cookie-message-banner__content">
+		<div class="warning-text" style="<?php echo esc_attr( $style_message ); ?>">
+			<p><?php esc_html_e( $options['content']['textarea_warning_text'], 'custom-cookie-message' ); ?>
+				<a style="<?php echo esc_attr( $style_link ); ?>" href="<?php echo esc_url( $options['general']['cookies_page_link'] ); ?>" title="<?php esc_html_e( $options['content']['input_link_text'], 'custom-cookie-message' ); ?>"><?php esc_html_e( $options['content']['input_link_text'], 'custom-cookie-message' ); ?></a>
 			</p>
-			<a id="cookies-button-ok" class="cookies-button-ok <?php echo ! empty( $options['styling']['add_button_class'] ) ? esc_attr( $options['styles']['add_button_class'] ) : esc_attr( 'default-cookie-button-style' ); ?>">
+			<a id="custom-cookie-message-preference" class="button <?php echo esc_attr( $style_button_class ); ?>">
 				<?php esc_html_e( $options['content']['input_button_text'], 'custom-cookie-message' ); ?>
 			</a>
 		</div>
 	</div>
 </div>
-<div id="custom-cookie-message-modal">
-
-</div>
+<div id="custom-cookie-message-modal"></div>
