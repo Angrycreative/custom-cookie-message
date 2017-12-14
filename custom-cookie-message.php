c6b248312c3aaa87d0db6d6a3228036b7302cb03
Luigi Guevara <luigi.guevara@angrycreative.se>
2017-12-14 21:24:31 +0100

    Updating plugin meta description

--- custom-cookie-message.php
+++ custom-cookie-message.php
@@ -2,29 +2,31 @@
 /**
  * Custom Cookie Message.
  *
- * @author            Luigi Guevara @killua99 & Angry Creative AB
- * @link              https://git.synotio.se/ac-components/ac-private-files.git
+ * @author            Angry Creative AB
+ * @link              https://github.com/Angrycreative/custom-cookie-message
  * @since             2.0.0
  * @package           CustomCookieMessage
  *
  * @wordpress-plugin
  * Plugin Name:       Custom Cookie Message
- * Plugin URI:        https://git.synotio.se/ac-components/ac-private-files.git
- * Description:       A customizable cookie message supported with GDPR.
+ * Plugin URI:        https://github.com/Angrycreative/custom-cookie-message
+ * Description:       A customizable cookie message, compatible with GDPR compliance.
  * Text Domain:       custom-cookie-message
  * Version:           2.0.0
  * Tested up to:      4.9
- * Author:            Luigi Guevara @killua99 & Angry Creative AB
+ * Requires PHP:      5.6
+ * Author:            Angry Creative AB
+ * Contributors:      Johan Sylvan, kylegard, Luigi Guevara @killua99
  * Author URI:        https://angrycreative.se/
  * License:           GPL-2.0+
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-use CustomCookieMessage\Main;
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+use CustomCookieMessage\Main;
 
 define( 'CUSTOM_COOKIE_MESSAGE_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'CUSTOM_COOKIE_MESSAGE_PLUGIN_URL', plugins_url( '', __FILE__ ) . '/' );
